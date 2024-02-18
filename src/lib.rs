324c1018201e73765991b8e60430780263112370
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-18 09:39:56 +0000

    Log "Hello, World!" to console

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use wasm_bindgen::prelude::wasm_bindgen;
+use gloo_console::log;
 
 #[wasm_bindgen]
 pub fn start() {
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
 
-    println!("Hello, world!");
+    log!("Hello, World!");
 }
