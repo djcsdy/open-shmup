ff2c86e2e3b6b32e3d9c3a3aff5b6dfcc3caa64c
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-18 08:50:14 +0000

    Build as a library and bind start function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-fn main() {
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[wasm_bindgen]
+pub fn start() {
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
 
