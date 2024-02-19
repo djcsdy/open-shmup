d5d38f2e12fd47b64587a2634e53eff68eef994a
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-19 07:42:09 +0000

    import: take name of file to read as a command line argument

--- import/src/main.rs
+++ import/src/main.rs
@@ -1,8 +1,16 @@
 use std::fs::File;
+
 use byteorder::{LittleEndian, ReadBytesExt};
+use clap::Parser;
+
+use crate::options::Options;
+
+mod options;
 
 fn main() {
-    let mut file = File::open("TODO").unwrap();
+    let options = Options::parse();
+
+    let mut file = File::open(options.path).unwrap();
     let signature = file.read_u16::<LittleEndian>().unwrap();
     if signature != 0x42 {
         panic!("Not a SEUCK game");
---
+++ import/src/options.rs
@@ -0,0 +1,6 @@
+use clap::Parser;
+
+#[derive(Parser)]
+pub struct Options {
+    pub path: String,
+}
