307207e74b4611157606257818eb3f88d4ffff73
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-19 10:59:51 +0000

    Add Game struct representing the data required for an open-shmup game

--- data/src/lib.rs
+++ data/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub struct Game {
+    pub background_scroll_data: [u8; 4096],
+    pub block_colours: [u8; 128],
+    pub block_data: [u8; 3072],
+    pub object_pointers: [u8; 1412],
+    pub title_screen: [u8; 480],
+    pub attack_wave_patterns: [u8; 3100],
+    pub sound_effects: [u8; 23296],
+    pub sprite_graphics: [u8; 8192],
+    pub background_tiles: [u8; 2040],
 }
