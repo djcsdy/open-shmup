65d106aa869e594af72c75dc08bfa9b3b2f9f582
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-19 07:37:14 +0000

    import: Open a file and check its signature

--- import/src/main.rs
+++ import/src/main.rs
@@ -1,3 +1,10 @@
+use std::fs::File;
+use byteorder::{LittleEndian, ReadBytesExt};
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("TODO").unwrap();
+    let signature = file.read_u16::<LittleEndian>().unwrap();
+    if signature != 0x42 {
+        panic!("Not a SEUCK game");
+    }
 }
