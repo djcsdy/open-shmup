c1a1a57974d5d084f48bf7bb78ad076f23cac966
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-20 00:38:39 +0000

    Implement read function for native open_shmup game format

--- data/src/lib.rs
+++ data/src/lib.rs
@@ -1,3 +1,8 @@
+use std::io;
+use std::io::{ErrorKind, Read};
+
+use byteorder::{LittleEndian, ReadBytesExt};
+
 pub struct Game {
     pub background_scroll_data: [u8; 4096],
     pub block_colours: [u8; 128],
@@ -8,4 +13,61 @@ pub struct Game {
     pub sound_effects: [u8; 2432],
     pub sprite_graphics: [u8; 8192],
     pub background_tiles: [u8; 2040],
+}
+
+const SIGNATURE: &[u8; 20] = b"\x00open_shmup_game\xff\xfe\xfd\xfc";
+
+impl Game {
+    pub fn read<R: Read>(reader: &mut R) -> io::Result<Self> {
+        let mut signature = [0u8; SIGNATURE.len()];
+        reader.read_exact(&mut signature)?;
+
+        if signature != *SIGNATURE {
+            return Err(io::Error::from(ErrorKind::InvalidData));
+        }
+
+        let version = reader.read_u32::<LittleEndian>()?;
+        if version != 1 {
+            return Err(io::Error::from(ErrorKind::InvalidData));
+        }
+
+        let mut background_scroll_data = [0u8; 4096];
+        reader.read_exact(&mut background_scroll_data)?;
+
+        let mut block_colours = [0u8; 128];
+        reader.read_exact(&mut block_colours)?;
+
+        let mut block_data = [0u8; 3072];
+        reader.read_exact(&mut block_data)?;
+
+        let mut object_pointers = [0u8; 1412];
+        reader.read_exact(&mut object_pointers)?;
+
+        let mut title_screen = [0u8; 480];
+        reader.read_exact(&mut title_screen)?;
+
+        let mut attack_wave_patterns = [0u8; 3100];
+        reader.read_exact(&mut attack_wave_patterns)?;
+
+        let mut sound_effects = [0u8; 2432];
+        reader.read_exact(&mut sound_effects)?;
+
+        let mut sprite_graphics = [0u8; 8192];
+        reader.read_exact(&mut sprite_graphics)?;
+
+        let mut background_tiles = [0u8; 2040];
+        reader.read_exact(&mut background_tiles)?;
+
+        Ok(Self {
+            background_scroll_data,
+            block_colours,
+            block_data,
+            object_pointers,
+            title_screen,
+            attack_wave_patterns,
+            sound_effects,
+            sprite_graphics,
+            background_tiles,
+        })
+    }
 }
