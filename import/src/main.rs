7f6bc572e1d393c3a12b8dd1df7e380aa837d5d3
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-20 01:26:04 +0000

    import: Read in a file in C64 PRG format, and write it out in openshmup format

--- import/src/main.rs
+++ import/src/main.rs
@@ -1,4 +1,5 @@
-use std::fs::File;
+use std::fs::{File, OpenOptions};
+use std::path::Path;
 
 use clap::Parser;
 
@@ -11,6 +12,20 @@ mod options;
 fn main() {
     let options = Options::parse();
 
-    let mut file = File::open(options.path).unwrap();
-    let game = Game::read_c64_prg(&mut file).unwrap();
+    let in_path = Path::new(&options.path);
+    let out_path = in_path.with_extension("openshmup");
+
+    if in_path == out_path {
+        panic!("Invalid input path");
+    }
+
+    let mut in_file = File::open(in_path).unwrap();
+    let game = Game::read_c64_prg(&mut in_file).unwrap();
+
+    let mut out_file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(out_path)
+        .unwrap();
+    game.write(&mut out_file).unwrap();
 }
