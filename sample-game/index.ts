a956b161658e5153c7d9a3947505b003b4e4ca19
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-25 23:27:12 +0000

    sample-game: ensure DOM is ready before starting game

--- sample-game/index.ts
+++ sample-game/index.ts
@@ -1,8 +1,9 @@
 import {start} from "open-shmup-engine";
+import domready from "domready";
 import * as gameUrl from "./atomsmash.openshmup";
 
 void fetch(gameUrl)
     .then(async response => response.blob())
     .then(async blob => blob.arrayBuffer())
     .then(async buffer => new Uint8Array(buffer))
-    .then(async game => start(game));
+    .then(async game => void domready(() => void start(game)));
