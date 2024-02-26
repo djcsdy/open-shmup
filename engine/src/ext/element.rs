cbe5fc66300333583e7c693903c382e0b7e294cf
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-26 01:41:18 +0000

    Add Element::into_svg_fe_color_matrix extension function

--- engine/src/ext/element.rs
+++ engine/src/ext/element.rs
@@ -1,10 +1,14 @@
 use wasm_bindgen::JsValue;
-use web_sys::{Element, HtmlCanvasElement, SvgDefsElement, SvgElement, SvgFilterElement};
+use web_sys::{
+    Element, HtmlCanvasElement, SvgDefsElement, SvgElement, SvgFilterElement,
+    SvgfeColorMatrixElement,
+};
 
 pub trait ElementExt {
     fn into_canvas(self) -> HtmlCanvasElement;
     fn into_svg(self) -> SvgElement;
     fn into_svg_defs(self) -> SvgDefsElement;
+    fn into_svg_fe_color_matrix(self) -> SvgfeColorMatrixElement;
     fn into_svg_filter(self) -> SvgFilterElement;
 }
 
@@ -16,8 +20,12 @@ impl ElementExt for Element {
     fn into_svg(self) -> SvgElement {
         JsValue::from(self).into()
     }
-    
+
     fn into_svg_defs(self) -> SvgDefsElement {
+        JsValue::from(self).into()
+    }
+
+    fn into_svg_fe_color_matrix(self) -> SvgfeColorMatrixElement {
         JsValue::from(self).into()
     }
 
