52ef6c1ec06395cb7b609c19a6521737e33bebc8
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-26 01:38:29 +0000

    Add Element::into_svg_filter extension function

--- engine/src/ext/element.rs
+++ engine/src/ext/element.rs
@@ -1,10 +1,11 @@
 use wasm_bindgen::JsValue;
-use web_sys::{Element, HtmlCanvasElement, SvgDefsElement, SvgElement};
+use web_sys::{Element, HtmlCanvasElement, SvgDefsElement, SvgElement, SvgFilterElement};
 
 pub trait ElementExt {
     fn into_canvas(self) -> HtmlCanvasElement;
     fn into_svg(self) -> SvgElement;
     fn into_svg_defs(self) -> SvgDefsElement;
+    fn into_svg_filter(self) -> SvgFilterElement;
 }
 
 impl ElementExt for Element {
@@ -17,6 +18,10 @@ impl ElementExt for Element {
     }
     
     fn into_svg_defs(self) -> SvgDefsElement {
+        JsValue::from(self).into()
+    }
+
+    fn into_svg_filter(self) -> SvgFilterElement {
         JsValue::from(self).into()
     }
 }
