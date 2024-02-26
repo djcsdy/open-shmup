b10bc62541e934b3e73cd7d1be5b49fdc77f136a
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-26 00:16:36 +0000

    Add Document::create_svg_element extension function

--- engine/src/ext/document.rs
+++ engine/src/ext/document.rs
@@ -1,12 +1,20 @@
 use crate::ext::element::ElementExt;
-use web_sys::{Document, HtmlCanvasElement};
+use crate::xml_namespace;
+use web_sys::{Document, HtmlCanvasElement, SvgElement};
 
 pub trait DocumentExt {
     fn create_canvas_element(&self) -> HtmlCanvasElement;
+    fn create_svg_element(&self) -> SvgElement;
 }
 
 impl DocumentExt for Document {
     fn create_canvas_element(&self) -> HtmlCanvasElement {
         self.create_element("canvas").unwrap().into_canvas()
+    }
+
+    fn create_svg_element(&self) -> SvgElement {
+        self.create_element_ns(Some(xml_namespace::SVG), "svg")
+            .unwrap()
+            .into_svg()
     }
 }
