858c473bd89d5a86e3ebf764d1f8a56af5a17b56
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-26 00:48:07 +0000

    Add Element::into_svg_defs extension function

--- engine/src/ext/element.rs
+++ engine/src/ext/element.rs
@@ -1,9 +1,10 @@
 use wasm_bindgen::JsValue;
-use web_sys::{Element, HtmlCanvasElement, SvgElement};
+use web_sys::{Element, HtmlCanvasElement, SvgDefsElement, SvgElement};
 
 pub trait ElementExt {
     fn into_canvas(self) -> HtmlCanvasElement;
     fn into_svg(self) -> SvgElement;
+    fn into_svg_defs(self) -> SvgDefsElement;
 }
 
 impl ElementExt for Element {
@@ -12,6 +13,10 @@ impl ElementExt for Element {
     }
 
     fn into_svg(self) -> SvgElement {
+        JsValue::from(self).into()
+    }
+    
+    fn into_svg_defs(self) -> SvgDefsElement {
         JsValue::from(self).into()
     }
 }
