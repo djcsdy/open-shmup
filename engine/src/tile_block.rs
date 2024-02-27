0c3320c429f45b8ce9a213d6ad96cd1bc562ca46
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-27 13:44:28 +0000

    Add TileBlock::draw function

--- engine/src/tile_block.rs
+++ engine/src/tile_block.rs
@@ -1,5 +1,6 @@
 use crate::palette_filter::PaletteFilter;
 use crate::tile::Tile;
+use web_sys::CanvasRenderingContext2d;
 
 #[derive(Clone)]
 pub struct TileBlock {
@@ -10,5 +11,16 @@ pub struct TileBlock {
 impl TileBlock {
     pub fn new(palette: PaletteFilter, tiles: [[Tile; 5]; 5]) -> Self {
         Self { palette, tiles }
+    }
+
+    pub fn draw(&self, context: &CanvasRenderingContext2d, x: f64, y: f64) {
+        context.save();
+        context.set_filter(self.palette.css());
+        for row in 0..5 {
+            for tile in 0..5 {
+                self.tiles[row][tile].draw(context, x + tile as f64 * 8.0, y + row as f64 * 8.0);
+            }
+        }
+        context.restore();
     }
 }
