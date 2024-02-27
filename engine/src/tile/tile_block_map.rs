374731f4584bb837832b095a7717b5f2ac90bd1d
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-27 20:58:56 +0000

    TileBlockMap: Use a heap-allocated Vec instead of stack-allocated array
    Ran out of space on the default stack!

--- engine/src/tile/tile_block_map.rs
+++ engine/src/tile/tile_block_map.rs
@@ -1,18 +1,21 @@
 use crate::tile::{TileBlock, TileBlockSet};
-use std::array;
 
 pub struct TileBlockMap {
-    tile_blocks: [[TileBlock; 8]; 512],
+    tile_blocks: Vec<Vec<TileBlock>>,
 }
 
 impl TileBlockMap {
     pub fn new(tile_block_set: TileBlockSet, map_data: &[u8; 4096]) -> Self {
         TileBlockMap {
-            tile_blocks: array::from_fn(|row_index| {
-                array::from_fn(|block_index| {
-                    tile_block_set[map_data[row_index * 8 + block_index] as usize].clone()
+            tile_blocks: (0..512)
+                .map(|row_index| {
+                    (0..8)
+                        .map(|block_index| {
+                            tile_block_set[map_data[row_index * 8 + block_index] as usize].clone()
+                        })
+                        .collect()
                 })
-            }),
+                .collect(),
         }
     }
 }
