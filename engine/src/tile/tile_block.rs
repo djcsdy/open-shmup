d0a5c1525f70dcf5bc5fb381ef6808e2083dd342
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-29 03:56:52 +0000

    Extract TileBlock::decode_multicolour from TileBlockSet::new

--- engine/src/tile/tile_block.rs
+++ engine/src/tile/tile_block.rs
@@ -1,11 +1,64 @@
-use web_sys::{CanvasRenderingContext2d, ImageBitmap};
+use crate::palette::SrgbPalette;
+use crate::tile::TileSet;
+use wasm_bindgen::Clamped;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{
+    window, CanvasRenderingContext2d, ImageBitmap, ImageBitmapOptions, ImageData, PremultiplyAlpha,
+};
 
 #[derive(Clone)]
 pub struct TileBlock(ImageBitmap);
 
 impl TileBlock {
-    pub fn new(bitmap: ImageBitmap) -> Self {
-        Self(bitmap)
+    pub async fn decode_multicolour(
+        tile_set: &TileSet,
+        palette: &SrgbPalette<4>,
+        tile_block_data: &[u8; 25],
+    ) -> Self {
+        let mut image_data_bytes = [0u8; 40 * 40 * 4];
+
+        for tile_index_in_block in 0..25 {
+            let tile_x_in_block = (tile_index_in_block % 5) * 8;
+            let tile_y_in_block = (tile_index_in_block / 5) * 8;
+            let tile_data = tile_set[tile_block_data[tile_index_in_block] as usize].tile_data();
+
+            for y_in_tile in 0..8 {
+                let line = tile_data[y_in_tile];
+                for x_in_tile in 0..4 {
+                    let out_x_l = tile_x_in_block + x_in_tile * 2;
+                    let out_x_r = out_x_l + 1;
+                    let out_y_pixel_offset = (tile_y_in_block + y_in_tile) * 40;
+                    let out_byte_offset_l = (out_y_pixel_offset + out_x_l) * 4;
+                    let out_byte_offset_r = (out_y_pixel_offset + out_x_r) * 4;
+                    let colour =
+                        palette[(line.wrapping_shr((6 - x_in_tile * 2) as u32) & 3) as usize];
+                    image_data_bytes[out_byte_offset_l] = colour.red();
+                    image_data_bytes[out_byte_offset_l + 1] = colour.green();
+                    image_data_bytes[out_byte_offset_l + 2] = colour.blue();
+                    image_data_bytes[out_byte_offset_l + 3] = 255;
+                    image_data_bytes[out_byte_offset_r] = colour.red();
+                    image_data_bytes[out_byte_offset_r + 1] = colour.green();
+                    image_data_bytes[out_byte_offset_r + 2] = colour.blue();
+                    image_data_bytes[out_byte_offset_r + 3] = 255;
+                }
+            }
+        }
+
+        Self(
+            JsFuture::from(
+                window()
+                    .unwrap()
+                    .create_image_bitmap_with_image_data_and_image_bitmap_options(
+                        &ImageData::new_with_u8_clamped_array(Clamped(&image_data_bytes), 40)
+                            .unwrap(),
+                        ImageBitmapOptions::new().premultiply_alpha(PremultiplyAlpha::None),
+                    )
+                    .unwrap(),
+            )
+            .await
+            .unwrap()
+            .into(),
+        )
     }
 
     pub fn draw(&self, context: &CanvasRenderingContext2d, x: f64, y: f64) {
--- engine/src/tile/tile_block_set.rs
+++ engine/src/tile/tile_block_set.rs
@@ -1,11 +1,9 @@
 use crate::palette::SrgbPalette;
 use crate::tile::TileBlock;
 use crate::tile::TileSet;
+use arrayref::array_ref;
 use futures::future;
 use std::ops::Index;
-use wasm_bindgen::Clamped;
-use wasm_bindgen_futures::JsFuture;
-use web_sys::{window, ImageBitmapOptions, ImageData, PremultiplyAlpha};
 
 #[derive(Clone)]
 pub struct TileBlockSet(Vec<TileBlock>);
@@ -18,63 +16,14 @@ impl TileBlockSet {
         tile_block_data: &[u8; 3200],
     ) -> Self {
         Self(
-            future::join_all(
-                (0..128)
-                    .map(|block_index| {
-                        let mut image_data_bytes = [0u8; 40 * 40 * 4];
-
-                        // FIXME: If the eighth bit is not set, we should render the block as hires
-                        let palette = &palettes[(block_colour_data[block_index] & 7) as usize];
-
-                        for tile_index_in_block in 0..25 {
-                            let tile_x_in_block = (tile_index_in_block % 5) * 8;
-                            let tile_y_in_block = (tile_index_in_block / 5) * 8;
-                            let tile_data = tile_set
-                                [tile_block_data[block_index * 25 + tile_index_in_block] as usize]
-                                .tile_data();
-
-                            for y_in_tile in 0..8 {
-                                let line = tile_data[y_in_tile];
-                                for x_in_tile in 0..4 {
-                                    let out_x_l = tile_x_in_block + x_in_tile * 2;
-                                    let out_x_r = out_x_l + 1;
-                                    let out_y_pixel_offset = (tile_y_in_block + y_in_tile) * 40;
-                                    let out_byte_offset_l = (out_y_pixel_offset + out_x_l) * 4;
-                                    let out_byte_offset_r = (out_y_pixel_offset + out_x_r) * 4;
-                                    let colour =
-                                        palette[(line.wrapping_shr((6 - x_in_tile * 2) as u32) & 3)
-                                            as usize];
-                                    image_data_bytes[out_byte_offset_l] = colour.red();
-                                    image_data_bytes[out_byte_offset_l + 1] = colour.green();
-                                    image_data_bytes[out_byte_offset_l + 2] = colour.blue();
-                                    image_data_bytes[out_byte_offset_l + 3] = 255;
-                                    image_data_bytes[out_byte_offset_r] = colour.red();
-                                    image_data_bytes[out_byte_offset_r + 1] = colour.green();
-                                    image_data_bytes[out_byte_offset_r + 2] = colour.blue();
-                                    image_data_bytes[out_byte_offset_r + 3] = 255;
-                                }
-                            }
-                        }
-
-                        JsFuture::from(
-                            window()
-                                .unwrap()
-                                .create_image_bitmap_with_image_data_and_image_bitmap_options(
-                                    &ImageData::new_with_u8_clamped_array(
-                                        Clamped(&image_data_bytes),
-                                        40,
-                                    )
-                                    .unwrap(),
-                                    ImageBitmapOptions::new()
-                                        .premultiply_alpha(PremultiplyAlpha::None),
-                                )
-                                .unwrap(),
-                        )
-                    })
-                    .map(move |image_data| async {
-                        TileBlock::new(image_data.await.unwrap().into())
-                    }),
-            )
+            future::join_all((0..128).map(|block_index| {
+                TileBlock::decode_multicolour(
+                    &tile_set,
+                    // FIXME: If the eighth bit is not set, we should render the block as hires
+                    &palettes[(block_colour_data[block_index] & 7) as usize],
+                    array_ref![tile_block_data, block_index * 25, 25],
+                )
+            }))
             .await,
         )
     }
