6c1c09c25e55e7afb5d523d985140a68351ee2d6
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-29 01:18:02 +0000

    Delete more unused SVG-related code

--- engine/src/ext/element.rs
+++ engine/src/ext/element.rs
@@ -1,35 +1,12 @@
 use wasm_bindgen::JsValue;
-use web_sys::{
-    Element, HtmlCanvasElement, SvgDefsElement, SvgElement, SvgFilterElement,
-    SvgfeColorMatrixElement,
-};
+use web_sys::{Element, HtmlCanvasElement};
 
 pub trait ElementExt {
     fn into_canvas(self) -> HtmlCanvasElement;
-    fn into_svg(self) -> SvgElement;
-    fn into_svg_defs(self) -> SvgDefsElement;
-    fn into_svg_fe_color_matrix(self) -> SvgfeColorMatrixElement;
-    fn into_svg_filter(self) -> SvgFilterElement;
 }
 
 impl ElementExt for Element {
     fn into_canvas(self) -> HtmlCanvasElement {
-        JsValue::from(self).into()
-    }
-
-    fn into_svg(self) -> SvgElement {
-        JsValue::from(self).into()
-    }
-
-    fn into_svg_defs(self) -> SvgDefsElement {
-        JsValue::from(self).into()
-    }
-
-    fn into_svg_fe_color_matrix(self) -> SvgfeColorMatrixElement {
-        JsValue::from(self).into()
-    }
-
-    fn into_svg_filter(self) -> SvgFilterElement {
         JsValue::from(self).into()
     }
 }
