fc3891675d78eb48c7b01175808559f1554f361a
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-03-03 23:04:44 +0000

    Extract Game struct

---
+++ engine/src/game/game.rs
@@ -0,0 +1,55 @@
+use crate::palette::SrgbPalette;
+use crate::screen::Screen;
+use crate::tile::{TileBlockMap, TileBlockSet, TileSet};
+use open_shmup_data::GameData;
+use wasm_bindgen::JsValue;
+use web_sys::CanvasRenderingContext2d;
+
+pub struct Game {
+    frame: u32,
+    screen: Screen,
+    palette: SrgbPalette<16>,
+    tile_block_map: TileBlockMap,
+}
+
+impl Game {
+    pub async fn new(data: GameData) -> Self {
+        let palette = SrgbPalette::new_colodore();
+        let tile_set = TileSet::new(&data.background_tiles);
+        let shared_tile_palette = palette.new_shared_tile_palette(&data.background_colours);
+        let tile_subpalettes = palette.new_tile_subpalettes(&shared_tile_palette);
+        let tile_block_set = TileBlockSet::new(
+            &tile_set,
+            &tile_subpalettes,
+            &data.block_colours,
+            &data.block_data,
+        )
+        .await;
+        let tile_block_map = TileBlockMap::new(&tile_block_set, &data.background_scroll_data);
+
+        Self {
+            screen: Screen::C64_PAL,
+            frame: 0,
+            palette,
+            tile_block_map,
+        }
+    }
+
+    pub fn update(&mut self, frame: u32) {
+        self.frame = frame;
+    }
+
+    pub fn draw(&self, context: &CanvasRenderingContext2d) {
+        context.set_fill_style(&JsValue::from(self.palette[0].css()));
+        context.fill_rect(
+            0.0,
+            0.0,
+            self.screen.width() as f64,
+            self.screen.height() as f64,
+        );
+
+        self.screen.with_play_area(context, |context| {
+            self.tile_block_map.draw(context, self.frame as i32);
+        })
+    }
+}
--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,5 +1,9 @@
 extern crate wee_alloc;
 
+use crate::ext::{DocumentExt, HtmlCanvasElementExt};
+use crate::game::Game;
+use crate::screen::Screen;
+use open_shmup_data::GameData;
 use std::cell::RefCell;
 use std::rc::Rc;
 use wasm_bindgen::closure::Closure;
@@ -8,15 +12,8 @@ use wasm_bindgen::{JsCast, JsValue};
 use web_sys::js_sys::{Date, Error};
 use web_sys::{window, CanvasRenderingContext2d, HtmlCanvasElement};
 
-use open_shmup_data::GameData;
-
-use crate::ext::{DocumentExt, HtmlCanvasElementExt};
-use crate::palette::SrgbPalette;
-use crate::screen::Screen;
-use crate::tile::TileSet;
-use crate::tile::{TileBlockMap, TileBlockSet};
-
 mod ext;
+mod game;
 mod palette;
 mod screen;
 mod stage;
@@ -44,38 +41,21 @@ pub async fn start(game: Vec<u8>, canvas: Option<HtmlCanvasElement>) -> Result<(
     canvas2.set_width(screen.width());
     canvas2.set_height(screen.height());
 
-    let context: CanvasRenderingContext2d = canvas2.get_context_2d().unwrap().unwrap();
-
-    let game =
+    let game_data =
         GameData::read(&mut game.as_slice()).map_err(|error| Error::new(&error.to_string()))?;
-
-    let palette = SrgbPalette::new_colodore();
-
-    context.set_fill_style(&JsValue::from(&palette[0].css()));
-    context.fill_rect(0.0, 0.0, 384.0, 288.0);
-
-    let tile_set = TileSet::new(&game.background_tiles);
-    let shared_tile_palette = palette.new_shared_tile_palette(&game.background_colours);
-    let tile_subpalettes = palette.new_tile_subpalettes(&shared_tile_palette);
-    let tile_block_set = TileBlockSet::new(
-        &tile_set,
-        &tile_subpalettes,
-        &game.block_colours,
-        &game.block_data,
-    )
-    .await;
-    let tile_block_map = TileBlockMap::new(&tile_block_set, &game.background_scroll_data);
+    let mut game = Game::new(game_data).await;
 
     let start_time = Date::now();
 
-    let on_animation_frame: Rc<RefCell<Option<Closure<dyn Fn() -> ()>>>> =
+    let context: CanvasRenderingContext2d = canvas2.get_context_2d().unwrap().unwrap();
+
+    let on_animation_frame: Rc<RefCell<Option<Closure<dyn FnMut() -> ()>>>> =
         Rc::new(RefCell::new(None));
     let on_animation_frame_2 = on_animation_frame.clone();
-    *on_animation_frame.borrow_mut() = Some(Closure::<dyn Fn() -> ()>::new(move || {
-        let frame = (Date::now() - start_time) / (1000.0 / 50.0);
-        screen.with_play_area(&context, |context| {
-            tile_block_map.draw(context, frame as i32);
-        });
+    *on_animation_frame.borrow_mut() = Some(Closure::<dyn FnMut() -> ()>::new(move || {
+        let frame = ((Date::now() - start_time) / (1000.0 / 50.0)) as u32;
+        game.update(frame);
+        game.draw(&context);
         window()
             .unwrap()
             .request_animation_frame(
