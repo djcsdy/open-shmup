5111f02bbd54ab32e2cc1d6fc92bbdf47fceefd6
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-03-04 02:38:58 +0000

    Add Rect::top_left function

--- engine/src/rect.rs
+++ engine/src/rect.rs
@@ -1,7 +1,18 @@
+use crate::point::Point;
+
 #[derive(Eq, PartialEq, Clone, Debug)]
 pub struct Rect {
     pub x: i32,
     pub y: i32,
     pub width: u32,
     pub height: u32,
+}
+
+impl Rect {
+    pub fn top_left(&self) -> Point {
+        Point {
+            x: self.x,
+            y: self.y,
+        }
+    }
 }
