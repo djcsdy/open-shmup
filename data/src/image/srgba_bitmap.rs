94f5501415ad7b29c824ee7e6f2c598fd65d9e45
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-03-12 23:36:32 +0000

    Add SrgbaBitmap::from_srgb_fn

--- data/src/image/srgba_bitmap.rs
+++ data/src/image/srgba_bitmap.rs
@@ -1,6 +1,32 @@
+use crate::palette::SrgbColour;
+
 #[derive(Eq, PartialEq, Clone, Hash)]
 pub struct SrgbaBitmap {
     pub width: usize,
     pub height: usize,
     pub bitmap: Vec<u8>,
+}
+
+impl SrgbaBitmap {
+    pub fn from_srgb_fn<F: FnMut(usize, usize) -> SrgbColour>(
+        width: usize,
+        height: usize,
+        mut f: F,
+    ) -> Self {
+        let mut bitmap = vec![0u8; width * height * 4];
+        for y in 0..height {
+            for x in 0..width {
+                let colour = f(x, y);
+                bitmap[(x + y * width) * 4] = colour.red();
+                bitmap[(x + y * width) * 4 + 1] = colour.green();
+                bitmap[(x + y * width) * 4 + 2] = colour.blue();
+                bitmap[(x + y * width) * 4 + 3] = 255;
+            }
+        }
+        Self {
+            width,
+            height,
+            bitmap,
+        }
+    }
 }
--- data/src/palette/mod.rs
+++ data/src/palette/mod.rs
@@ -3,5 +3,5 @@ mod pal_colour;
 mod srgb_colour;
 mod srgb_palette;
 
-use srgb_colour::SrgbColour;
+pub use srgb_colour::SrgbColour;
 pub use srgb_palette::SrgbPalette;
