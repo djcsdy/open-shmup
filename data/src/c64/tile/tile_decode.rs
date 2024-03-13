627a6c87fb01d55cbe8ebea4f2f8bbdcbcd06c05
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-03-13 05:18:27 +0000

    Add C64TileDecode::to_srgba_bitmap function

--- data/src/c64/tile/tile_decode.rs
+++ data/src/c64/tile/tile_decode.rs
@@ -1,5 +1,14 @@
+use crate::image::SrgbaBitmap;
+use crate::palette::SrgbPalette;
+
 pub trait C64TileDecode {
     fn width(&self) -> usize;
     fn height(&self) -> usize;
     fn colour_index_at(&self, x: usize, y: usize) -> u8;
+
+    fn to_srgba_bitmap(&self, palette: &SrgbPalette<4>) -> SrgbaBitmap {
+        SrgbaBitmap::from_srgb_fn(self.width(), self.height(), |x, y| {
+            palette[self.colour_index_at(x, y) as usize]
+        })
+    }
 }
