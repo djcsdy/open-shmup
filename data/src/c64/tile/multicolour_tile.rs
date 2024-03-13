f33d70aaa94d62745d1d10afc28c221786b8dda8
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-03-13 01:45:42 +0000

    Implement C64TileDecode for C64MulticolourTileData

--- data/src/c64/tile/multicolour_tile.rs
+++ data/src/c64/tile/multicolour_tile.rs
@@ -1,9 +1,17 @@
-use crate::c64::C64TileData;
+use crate::c64::{C64TileData, C64TileDecode};
 
 pub struct C64MulticolourTileData<'tile_data>(&'tile_data C64TileData);
 
 impl<'tile_data> C64MulticolourTileData<'tile_data> {
     pub fn new(tile_data: &'tile_data C64TileData) -> Self {
         Self(tile_data)
+    }
+}
+
+impl C64TileDecode for C64MulticolourTileData<'_> {
+    fn colour_index_at(&self, x: usize, y: usize) -> u8 {
+        let x_in_tile = x / 2;
+        let line = self.0 .0[y];
+        line.wrapping_shr((6 - x_in_tile * 2) as u32) & 3
     }
 }
