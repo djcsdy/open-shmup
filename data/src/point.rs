b974092feb482ffb9e65a1a6fb5f6a2a3d5718b5
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-03-18 03:56:08 +0000

    Implement Add for Point

--- data/src/point.rs
+++ data/src/point.rs
@@ -1,5 +1,18 @@
+use std::ops::Add;
+
 #[derive(Eq, PartialEq, Clone, Debug)]
 pub struct Point {
     pub x: i32,
     pub y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, rhs: Point) -> Self::Output {
+        Point {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
 }
